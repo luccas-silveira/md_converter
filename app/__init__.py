57d3d54bb730900e6b574f1638640ffe567d1e47
luccas-silveira <luccasrobertosilveira@gmail.com>
2025-09-14 19:55:39 -0300

    Não acredito que deu errado... pq lady gaga... pq?

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,31 @@ def create_app():
     """Factory function para criar a aplicação Flask"""
     app = Flask(__name__)
 
+    # Configurar prefixo para reverse proxy
+    class ReverseProxied(object):
+        def __init__(self, app, script_name=None, scheme=None, server=None):
+            self.app = app
+            self.script_name = script_name
+            self.scheme = scheme
+            self.server = server
+
+        def __call__(self, environ, start_response):
+            script_name = environ.get('HTTP_X_SCRIPT_NAME', '') or self.script_name
+            if script_name:
+                environ['SCRIPT_NAME'] = script_name
+                path_info = environ['PATH_INFO']
+                if path_info.startswith(script_name):
+                    environ['PATH_INFO'] = path_info[len(script_name):]
+            scheme = environ.get('HTTP_X_SCHEME', '') or self.scheme
+            if scheme:
+                environ['wsgi.url_scheme'] = scheme
+            server = environ.get('HTTP_X_FORWARDED_SERVER', '') or self.server
+            if server:
+                environ['HTTP_HOST'] = server
+            return self.app(environ, start_response)
+
+    app.wsgi_app = ReverseProxied(app.wsgi_app, script_name='/relatorio')
+
     # Configurações
     app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100MB max file size
     app.config['UPLOAD_FOLDER'] = '/tmp'
