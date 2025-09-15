4bf6b9a92dbc459b4a32516a33645e8f0150954a
luccas-silveira <luccasrobertosilveira@gmail.com>
2025-09-14 22:04:46 -0300

    só aumenta

--- app/routes/main.py
+++ app/routes/main.py
@@ -2,11 +2,12 @@
 Rotas principais da aplicação
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/")
 def index():
     """Página principal da aplicação"""
-    return render_template('front.html')
+    max_size_mb = int(current_app.config.get('MAX_CONTENT_LENGTH', 100 * 1024 * 1024) / (1024 * 1024))
+    return render_template('front.html', max_size_mb=max_size_mb)
